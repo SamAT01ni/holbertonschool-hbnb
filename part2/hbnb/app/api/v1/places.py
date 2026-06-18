473e72e0bfe704688d34bad9165d0fcae06da41f
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-18 12:59:13 +1000

    start of adding places api

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -1,1 +1,65 @@
 #!/usr/bin/python3
+
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('places', description='Place operations')
+
+# Define the models for related entities
+amenity_model = api.model('PlaceAmenity', {
+    'id': fields.String(description='Amenity ID'),
+    'name': fields.String(description='Name of the amenity')
+})
+
+user_model = api.model('PlaceUser', {
+    'id': fields.String(description='User ID'),
+    'first_name': fields.String(description='First name of the owner'),
+    'last_name': fields.String(description='Last name of the owner'),
+    'email': fields.String(description='Email of the owner')
+})
+
+# Define the place model for input validation and documentation
+place_model = api.model('Place', {
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(required=True, description='Latitude of the place'),
+    'longitude': fields.Float(required=True, description='Longitude of the place'),
+    'owner_id': fields.String(required=True, description='ID of the owner'),
+    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")
+})
+
+place_lamb1 = api.model('Place Marhsal', {
+    'id': fields.String,
+    'title': fields.String,
+    'latitude': fields.Float,
+    'longitude': fields.Float,
+})
+place_lamb2 = api.model('Place Marhsal', {
+    'id': fields.String,
+    'title': fields.String,
+    'description': fields.String,
+    'price': fields.Float,
+    'latitude': fields.Float,
+    'longitude': fields.Float,
+    'owner_id': fields.String,
+})
+
+
+@api.route('/')
+class PlaceList(Resource):
+    @api.expect(place_model)
+    @api.response(201, 'Place successfully created')
+    @api.response(400, 'Invalid input data')
+    @api.marshal_with(place_lamb2)
+    def post(self):
+        """Register a new place"""
+        place_data = api.payload
+        new_place = facade.create_place(place_data)
+        return new_place, 201
+
+    @api.response(200, 'List of places retrieved successfully')
+    def get(self):
+        """Retrieve a list of all places"""
+        places = facade.get_all_places()
+        return places
