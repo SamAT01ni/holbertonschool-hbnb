56957b2bd1915bb4aed974e7a6aae67af04d63e6
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-06-18 12:48:17 +1000

    marshaling added to amenities

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -10,26 +10,30 @@ api = Namespace('amenities', description='Amenity operations')
 amenity_model = api.model('Amenity', {
     'name': fields.String(required=True, description='Name of the amenity')
 })
+amenity_lamb = api.model('Amenity Marshal', {
+    'id': fields.String,
+    'name': fields.String,
+})
 
 @api.route('/')
 class AmenityList(Resource):
     @api.expect(amenity_model)
     @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
+    @api.marshal_with(amenity_lamb)
 
     def post(self):
         """Register a new amenity"""
         amenity_data = api.payload
         new_amenity = facade.create_amenity(amenity_data)
-        return {"id": new_amenity.id, "name": new_amenity.name}, 201
+        return new_amenity, 201
 
     @api.response(200, 'List of amenities retrieved successfully')
+    @api.marshal_list_with(amenity_lamb)
     def get(self):
         """Retrieve a list of all amenities"""
         amenities = facade.get_all_amenities()
-        return [{'id': amenity.id, 'name': amenity.name}
-                for amenity in amenities], 200
-
+        return amenities
 
 @api.route('/<amenity_id>')
 class AmenityResource(Resource):
@@ -47,10 +51,8 @@ class AmenityResource(Resource):
     @api.response(200, 'Amenity updated successfully')
     @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
-    
     def put(self, amenity_id):
         """Update an amenity's information"""
-
         updated_amenity= facade.update_amenity(amenity_id, api.payload)
         if not updated_amenity:
             return {'error': 'Amenity not found'}, 404
