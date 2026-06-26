d158f8b94ccd29cef8f3540dc94b449b682c501e
Zac <13089@holbertonstudents.com.au>
2026-06-26 09:20:49 +0200

    added jwt extension to extension file

--- part3/hbnb/app/extensions.py
+++ part3/hbnb/app/extensions.py
@@ -2,5 +2,7 @@
 """Application extensions to stop circular import during test."""
 
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
