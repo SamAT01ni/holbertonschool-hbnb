c8e6de65c5240dbb85d3bb85c24c397d991f0387
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-11 13:51:00 +1000

    fixed integer error and added constraint for rating values

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -11,7 +11,9 @@ class Review(BaseModel):
     """Review class for place reviews."""
     __tablename__ = 'reviews'
 
+    __table_args__ = (db.CheckConstraint('rating >= 1 AND rating <= 5', name='check_rating_range'),)
+
     text = db.Column(db.String(256), nullable=False)
-    rating = db.Column(db.Integer(5), nullable=False)
+    rating = db.Column(db.Integer(), nullable=False)
     user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
     place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
